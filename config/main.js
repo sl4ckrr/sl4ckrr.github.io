dda0b94baff5eaca12ef2b82b792f79ff6ed4061
Rhap5ody <domingogramos808@gmail.com>
2024-11-26 21:21:35 -0600

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,3 +1,17 @@
+const input = document.querySelector('input');
+const games = document.querySelectorAll('#games img');
+
+input.addEventListener('input', () => {
+  const searchTerm = input.value.toLowerCase();
+  games.forEach(game => {
+    if (game.alt.toLowerCase().includes(searchTerm)) {
+      game.style.display = 'block';
+    } else {
+      game.style.display = 'none';
+    }
+  });
+});
+
 // This changes the title the website
 
 var sitename = "Rhap5ody"; // Change this to change the name of your website.
